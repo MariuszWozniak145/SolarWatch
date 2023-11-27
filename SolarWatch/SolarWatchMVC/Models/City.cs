0193f6524b487349edf7b46a465efe42329a5ee5
Mariusz Woźniak <mariuszwozniak145@gmail.com>
2023-11-27 22:39:41 +0100

    Added City and SunriseSunsetTimes models

---
+++ SolarWatch/SolarWatchMVC/Models/City.cs
@@ -0,0 +1,22 @@
+﻿namespace SolarWatchMVC.Models;
+
+public class City
+{
+    public Guid Id { get; init; }
+    public string Name { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public List<SunriseSunsetTimes> SunriseSunsetTimes { get; init; } = new();
+
+    public City(string name, string? state, string? country, double latitude, double longitude)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        State = state;
+        Country = country;
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+}
---
+++ SolarWatch/SolarWatchMVC/Models/SunriseSunsetTimes.cs
@@ -0,0 +1,20 @@
+﻿namespace SolarWatchMVC.Models;
+
+public class SunriseSunsetTimes
+{
+    public Guid Id { get; init; }
+    public City City { get; init; }
+    public Guid CityId { get; init; }
+    public string Date { get; init; }
+    public string Sunrise { get; init; }
+    public string Sunset { get; init; }
+
+    public SunriseSunsetTimes(string date, Guid cityId, string sunrise, string sunset)
+    {
+        Id = Guid.NewGuid();
+        CityId = cityId;
+        Date = date;
+        Sunrise = sunrise;
+        Sunset = sunset;
+    }
+}
